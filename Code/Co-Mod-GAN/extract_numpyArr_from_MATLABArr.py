f059ea0c0ef9f08e29f8c5b560d608cbf617d497
Guy-Shtainer <79972955+Guy-Shtainer@users.noreply.github.com>
2023-08-31 16:00:37 +0300

    Update extract_numpyArr_from_MATLABArr.py

--- Code/Co-Mod-GAN/extract_numpyArr_from_MATLABArr.py
+++ Code/Co-Mod-GAN/extract_numpyArr_from_MATLABArr.py
@@ -1,8 +1,8 @@
 from scipy.io import loadmat
 
-def extract_numpyArr_from_MATLABArr(matlabFileFolderPath):
+def extract_numpyArr_from_MATLABArr(matlabFilePath):
     '''
-    This function receives the full path of the matlab file which contains the exact calculation of the correlation
+    This function receives the full path of the Matlab file which contains the exact calculation of the correlation
     matrices. In order to extract matrix #d from the returned value "matrices", use the following format: matrices[
     matrix + "%d"][0,0].
 
@@ -10,7 +10,7 @@ def extract_numpyArr_from_MATLABArr(matlabFileFolderPath):
             matlabFileFolderPath (str): Path to the folder in which corrMatrices.mat file is in.
 
         Returns:
-            matrices: Array of matrices which corresponds to the energy lattices.
+            matrices: Array of matrices that correspond to the energy lattices.
         '''
-    matrices = loadmat(matlabFileFolderPath + '/corrMatrices.mat')
+    matrices = loadmat(matlabFilePath + '/corrMatrices.mat')
     return matrices['matrix_struct']
